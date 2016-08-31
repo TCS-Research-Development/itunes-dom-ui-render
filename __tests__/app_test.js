aeed611edda7ecbe4da51a54fee5a5bfa074a3df
Anish U <Anish@Anishs-MacBook-Pro.local>
2016-08-31 13:03:24 +0530

    Added files in Release#2 including some test case

---
+++ __tests__/app_test.js
@@ -0,0 +1,13 @@
+jest.autoMockOff();
+var React = require('react');
+var ReactDOM = require ('react-dom');
+var TestUtils = require('react-addons-test-utils');
+const App = require('../src/app'); 
+
+describe('App', () => {
+    
+
+  
+  it('', () => {
+  expect("true").toEqual('true');
+}); })
